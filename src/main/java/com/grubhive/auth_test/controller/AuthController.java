7e92ec5acfcf4c51b5e73d768b7e50daed9021ca
Oscar Nwanze <nwanzeoscar@gmail.com>
2025-03-24 22:37:19 +0100

    minor AuthController update

--- src/main/java/com/grubhive/auth_test/controller/AuthController.java
+++ src/main/java/com/grubhive/auth_test/controller/AuthController.java
@@ -5,14 +5,16 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+import org.springframework.security.core.Authentication;
 
 
 @RestController
 @RequestMapping("/api/demo")
 public class AuthController {
     @GetMapping
-    public java.util.Map<String, String> getUserInfo() {
-        org.springframework.security.core.Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    public Map<String, String> getUserInfo() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         return java.util.Map.of("User", auth.getName(), "Role", auth.getAuthorities().iterator().next().getAuthority());
     }
 }
